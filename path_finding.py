68fa27ca6e1218ebee6d8abce43585efc0d14b23
oran hayes <hayesor@tcd.ie>
2025-12-01 14:51:50 +0000

    changed obstacle detect

--- path_finding.py
+++ path_finding.py
@@ -85,7 +85,7 @@ def read_path_waypoints(csv_file='path.csv'):
         normalized_waypoints = []
         for point in waypoints:
             # Convert to tuples with height 0.4
-            normalized_waypoints.append(((point[0] - offset_x)/10, -(point[1] - offset_y)/10, 0.23))
+            normalized_waypoints.append(((point[0] - offset_x)/10, -(point[1] - offset_y)/10, 0.18))
         return normalized_waypoints
     
     return waypoints
@@ -113,7 +113,7 @@ def read_all_blocks(csv_file='path.csv'):
     normalized_blocks = []
     for block in blocks:
         # Convert to tuples with height 4
-        normalized_blocks.append(((block[0] - offset_x)/10, -(block[1] - offset_y)/10, 0.23))
+        normalized_blocks.append(((block[0] - offset_x)/10, -(block[1] - offset_y)/10, 0.18))
     
     return normalized_blocks
 
@@ -135,9 +135,9 @@ def param_deck_flow(_, value_str):
         flow_deck_attached_event.set()
 
 def over_obstacle():
-    global shutter_readings
-    if len(shutter_readings) < MAX_READINGS:
-        return False  # Not enough readings yet
+    global shutter_num
+    return shutter_num > COLOUR_THRESHOLD
+
     
     # Calculate moving average of last 10 readings
     moving_average = sum(shutter_readings) / len(shutter_readings)
@@ -191,20 +191,19 @@ def run_sequence(scf, sequence, base_x, base_y, base_z, yaw):
 
         print(f"Reached position {position}")
         
-
+        # Check for obstacle over multiple readings     
+        black = 0
         for i in range(50):
             if over_obstacle():
-                print("Obstacle detected!")
-                obstacle_detected = True
-                for i in range(20):
-                    cf.commander.send_position_setpoint(initial_position[0], initial_position[1], 0.23, yaw)
-                    time.sleep(0.1)
-                update_csv(position, prior_position)
-                break
+                # print("Over black")
+                black = black + 1
             time.sleep(0.01)
 
-        if obstacle_detected:
-            break
+
+        print(f"Black count: {black}")
+        if black >= 40:
+            print("Obstacle detected!")
+ 
 
     cf.commander.send_stop_setpoint()
     # Hand control over to the high level commander to avoid timeout and locking of the Crazyflie
@@ -215,20 +214,15 @@ def run_sequence(scf, sequence, base_x, base_y, base_z, yaw):
     time.sleep(0.1)
 
 def log_stab_callback(timestamp, data, logconf):
-    global estimated_position, shutter_num, shutter_readings
+    global estimated_position, shutter_num
     estimated_position[0] = data['stateEstimate.x']
     estimated_position[1] = data['stateEstimate.y']
     estimated_position[2] = data['stateEstimate.z']
     shutter_num = data['motion.shutter']
     
     # Add new reading to the list
-    shutter_readings.append(shutter_num)
     
-    # Keep only the last MAX_READINGS readings
-    if len(shutter_readings) > MAX_READINGS:
-        shutter_readings.pop(0)
-
-    if timestamp % 200 == 0:  # Print every 200th callback to reduce output
+    if timestamp % 2000 == 0:  # Print every 200th callback to reduce output
         print('[%d][%s]: %s' % (timestamp, logconf.name, data))
 
 def simple_log_async(scf, logconf):
